694e74d92d45e87999fef980cf9385a3eef2a387
satish.thapa <satish.thapa@innovatetech.co>
2024-09-04 17:52:49 +0545

    uncommited bulk-export

--- TESTPAPER/bulk-test-script.js
+++ TESTPAPER/bulk-test-script.js
@@ -30,38 +30,38 @@ export const options = {
 }
 
 export default function () {
-  // group("Peform bulk export", () => {
-  //   let response = login(__ENV.USERNAME, __ENV.PASSWORD)
+  group("Peform bulk export", () => {
+    let response = login(__ENV.USERNAME, __ENV.PASSWORD)
 
-  //   const token = JSON.parse(response.body).data.login.token
+    const token = JSON.parse(response.body).data.login.token
 
-  //   const isLoginSucessFull = checkSucessStatusCode(response)
+    const isLoginSucessFull = checkSucessStatusCode(response)
 
-  //   if (isLoginSucessFull) {
-  //     sleep(2)
+    if (isLoginSucessFull) {
+      sleep(2)
 
-  //     execCommonRequest(token)
+      execCommonRequest(token)
 
-  //     const bulkExportResponse = http.post(
-  //       "https://api-devtestpaper.innovatetech.io/graphql",
-  //       '{"operationName":"bulkExportQuestions","variables":{"bucketId":"592d0207-a297-40fc-85a2-2c5fc0f6ab24"},"query":"mutation bulkExportQuestions($bucketId: String, $testpaperId: String) {\\n  bulkExportQuestions(bucketId: $bucketId, testpaperId: $testpaperId) {\\n    bulkExportId\\n    __typename\\n  }\\n}\\n"}',
-  //       {
-  //         headers: {
-  //           authorization: token,
-  //           "content-type": "application/json",
-  //         },
-  //       }
-  //     )
-  //     const bulkExportId = JSON.parse(bulkExportResponse.body).data
-  //       .bulkExportQuestions.bulkExportId
+      const bulkExportResponse = http.post(
+        "https://api-devtestpaper.innovatetech.io/graphql",
+        '{"operationName":"bulkExportQuestions","variables":{"bucketId":"592d0207-a297-40fc-85a2-2c5fc0f6ab24"},"query":"mutation bulkExportQuestions($bucketId: String, $testpaperId: String) {\\n  bulkExportQuestions(bucketId: $bucketId, testpaperId: $testpaperId) {\\n    bulkExportId\\n    __typename\\n  }\\n}\\n"}',
+        {
+          headers: {
+            authorization: token,
+            "content-type": "application/json",
+          },
+        }
+      )
+      const bulkExportId = JSON.parse(bulkExportResponse.body).data
+        .bulkExportQuestions.bulkExportId
 
-  //     pollForCompletion(token, bulkExportId, "bulkExport", bulkExportTrend)
+      pollForCompletion(token, bulkExportId, "bulkExport", bulkExportTrend)
 
-  //     sleep(1)
-  //   } else {
-  //     console.error("Login failed")
-  //   }
-  // })
+      sleep(1)
+    } else {
+      console.error("Login failed")
+    }
+  })
   group("Peform bulk import", () => {
     let response = login(__ENV.USERNAME, __ENV.PASSWORD)
 
